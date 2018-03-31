7137dfe79b6468668420a7b48c2da491d545c51e
Demo User <debian@beaglebone.localdomain>
2018-03-31 00:49:26 +0000

    Data collection now works

--- dataLogging.py
+++ dataLogging.py
@@ -1,35 +1,47 @@
 #!/usr/bin/python
 #This program handles reading the output of the various sensor and writing the output
-#to a file
+#to a f
+import time, sys
 
-dataFile = open("data.csv","w+")
-dataFile.write("Acceleration,Pressure,Temperature,Altitude\n")
-rawXAccelFile = open('/sys/bus/iio/devices/iio:device1/in_accel_x_raw')
-accelScaleFile = open('/sys/bus/iio/devices/iio:device1/in_accel_scale')
-rawPressureFile = open('/sys/bus/iio/devices/iio:device2/in_pressure_raw')
-pressureScaleFile = open('/sys/bus/iio/devices/iio:device2/in_pressure_scale')
-tempOffsetFile = open('/sys/bus/iio/devices/iio:device2/in_temp_offset')
-rawTempFile = open('/sys/bus/iio/devices/iio:device2/in_temp_raw')
-tempScaleFile = open('/sys/bus/iio/devices/iio:device2/in_temp_scale')
-rawXAccel = rawXAccelFile.read()
-accelScale =  accelScaleFile.read()
-rawPressure = rawPressureFile.read()
-pressureScale = pressureScaleFile.read()
-tempOffset = tempOffsetFile.read()
-rawTemp = rawTempFile.read()
-tempScale = tempScaleFile.read()
+def main(dataFile,  rawXAccelFile,  accelScaleFile,  rawPressureFile,  pressureScaleFile,  tempOffsetFile,  rawTempFile,  tempScaleFile):
+    print "Starting data collection"
+    rawXAccel = rawXAccelFile.read()
+    accelScale =  accelScaleFile.read()
+    rawPressure = rawPressureFile.read()
+    pressureScale = pressureScaleFile.read()
+    tempOffset = tempOffsetFile.read()
+    rawTemp = rawTempFile.read()
+    tempScale = tempScaleFile.read()
+    try: 
+        xAccel = int(rawXAccel) * float(accelScale) * 9.81
+    except ValueError:
+        pass
+    try:
+        pressure = int(rawPressure) * float(pressureScale) * 10 
+    except ValueError:
+        pass
+    try:
+        temperature = float(tempOffset) + (int(rawTemp) * float(tempScale)) 
+    except ValueError:
+        pass
+    try:
+        altitude = (1 - ((pressure/1013.25) ** 0.190284)) * 145366.45 
+    except ValueError:
+        pass
+    dataFile.write(str(xAccel) + "," + str(pressure) + "," + str(temperature) + "," + str(altitude) + "\n")
 
-xAccel = int(rawXAccel) * float(accelScale)
-pressure = int(rawPressure) * float(pressureScale) * 10
-temperature = float(tempOffset) + (int(rawTemp) * float(tempScale))
-altitude = (1 - ((pressure/1013.25) ** 0.190284)) * 145366.45
-dataFile.write(str(xAccel) + "," + str(pressure) + "," + str(temperature) + "," + str(altitude) + "\n")
+def cleanup(dataFile):
+    print "Starting cleanup"
+    dataFile.close()
+    sys.exit(1)
 
-rawXAccelFile.close()
-accelScaleFile.close()
-rawPressureFile.close()
-pressureScaleFile.close()
-tempOffsetFile.close()
-rawTempFile.close()
-tempScaleFile.close()
-dataFile.close()
+if __name__ == '__main__':
+    try:
+        dataFile = open("data.csv","w+")
+        dataFile.write("Acceleration,Pressure,Temperature,Altitude\n")
+        while True:
+            with open('/sys/bus/iio/devices/iio:device1/in_accel_x_raw', 'r') as rawXAccelFile, open('/sys/bus/iio/devices/iio:device1/in_accel_scale', 'r') as accelScaleFile, open('/sys/bus/iio/devices/iio:device2/in_pressure_raw', 'r') as rawPressureFile, open('/sys/bus/iio/devices/iio:device2/in_pressure_scale', 'r') as pressureScaleFile, open('/sys/bus/iio/devices/iio:device2/in_temp_offset', 'r') as tempOffsetFile, open('/sys/bus/iio/devices/iio:device2/in_temp_raw', 'r') as rawTempFile, open('/sys/bus/iio/devices/iio:device2/in_temp_scale', 'r') as tempScaleFile:
+                main(dataFile,  rawXAccelFile,  accelScaleFile,  rawPressureFile,  pressureScaleFile,  tempOffsetFile,  rawTempFile,  tempScaleFile)
+    except KeyboardInterrupt:
+        cleanup(dataFile)
+ 
