e6051ca7e88c7c2c83ea1979e2e4594144e8c057
nickjgski <33236986+nickjgski@users.noreply.github.com>
2018-03-30 22:48:10 -0400

    Reverted to not having pyinotify

--- dataLogging.py
+++ dataLogging.py
@@ -3,13 +3,13 @@
 #to a f
 import time, sys, pyinotify
 
-class EventHandler(pyinotify.ProcessEvent):
-    def my_init(self, file_object=sys.stdout):
-        self._file_object = file_object
+#class EventHandler(pyinotify.ProcessEvent):
+#    def my_init(self, file_object=sys.stdout):
+#        self._file_object = file_object
 
-    def process_default(self, event):
-        with open('/sys/bus/iio/devices/iio:device1/in_accel_x_raw', 'r') as rawXAccelFile, open('/sys/bus/iio/devices/iio:device1/in_accel_scale', 'r') as accelScaleFile, open('/sys/bus/iio/devices/iio:device2/in_pressure_raw', 'r') as rawPressureFile, open('/sys/bus/iio/devices/iio:device2/in_pressure_scale', 'r') as pressureScaleFile, open('/sys/bus/iio/devices/iio:device2/in_temp_offset', 'r') as tempOffsetFile, open('/sys/bus/iio/devices/iio:device2/in_temp_raw', 'r') as rawTempFile, open('/sys/bus/iio/devices/iio:device2/in_temp_scale', 'r') as tempScaleFile:
-            main(dataFile,  rawXAccelFile,  accelScaleFile,  rawPressureFile,  pressureScaleFile,  tempOffsetFile,  rawTempFile,  tempScaleFile)
+#    def process_default(self, event):
+#        with open('/sys/bus/iio/devices/iio:device1/in_accel_x_raw', 'r') as rawXAccelFile, open('/sys/bus/iio/devices/iio:device1/in_accel_scale', 'r') as accelScaleFile, open('/sys/bus/iio/devices/iio:device2/in_pressure_raw', 'r') as rawPressureFile, open('/sys/bus/iio/devices/iio:device2/in_pressure_scale', 'r') as pressureScaleFile, open('/sys/bus/iio/devices/iio:device2/in_temp_offset', 'r') as tempOffsetFile, open('/sys/bus/iio/devices/iio:device2/in_temp_raw', 'r') as rawTempFile, open('/sys/bus/iio/devices/iio:device2/in_temp_scale', 'r') as tempScaleFile:
+#            main(dataFile,  rawXAccelFile,  accelScaleFile,  rawPressureFile,  pressureScaleFile,  tempOffsetFile,  rawTempFile,  tempScaleFile)
 
 def main(dataFile,  rawXAccelFile,  accelScaleFile,  rawPressureFile,  pressureScaleFile,  tempOffsetFile,  rawTempFile,  tempScaleFile):
     print "Starting data collection"
@@ -47,14 +47,14 @@ if __name__ == '__main__':
     try:
         dataFile = open("data.csv","w+")
         dataFile.write("Acceleration,Pressure,Temperature,Altitude\n")
-        ##while True:
-        ##    with open('/sys/bus/iio/devices/iio:device1/in_accel_x_raw', 'r') as rawXAccelFile, open('/sys/bus/iio/devices/iio:device1/in_accel_scale', 'r') as accelScaleFile, open('/sys/bus/iio/devices/iio:device2/in_pressure_raw', 'r') as rawPressureFile, open('/sys/bus/iio/devices/iio:device2/in_pressure_scale', 'r') as pressureScaleFile, open('/sys/bus/iio/devices/iio:device2/in_temp_offset', 'r') as tempOffsetFile, open('/sys/bus/iio/devices/iio:device2/in_temp_raw', 'r') as rawTempFile, open('/sys/bus/iio/devices/iio:device2/in_temp_scale', 'r') as tempScaleFile:
-        ##        main(dataFile,  rawXAccelFile,  accelScaleFile,  rawPressureFile,  pressureScaleFile,  tempOffsetFile,  rawTempFile,  tempScaleFile)
-        wm = pyinotify.WatchManager()
-        notifier = pyinotify.Notifier(wm, EventHandler())
-        wm.add_watch('/sys/bus/iio/devices/iio:device1/', pyinotify.ALL_EVENTS)
-        wm.add_watch('/sys/bus/iio/devices/iio:device2/', pyinotify.ALL_EVENTS)
-        p = EventHandler("data.csv")
-        notifier.loop()
+        while True:
+            with open('/sys/bus/iio/devices/iio:device1/in_accel_x_raw', 'r') as rawXAccelFile, open('/sys/bus/iio/devices/iio:device1/in_accel_scale', 'r') as accelScaleFile, open('/sys/bus/iio/devices/iio:device2/in_pressure_raw', 'r') as rawPressureFile, open('/sys/bus/iio/devices/iio:device2/in_pressure_scale', 'r') as pressureScaleFile, open('/sys/bus/iio/devices/iio:device2/in_temp_offset', 'r') as tempOffsetFile, open('/sys/bus/iio/devices/iio:device2/in_temp_raw', 'r') as rawTempFile, open('/sys/bus/iio/devices/iio:device2/in_temp_scale', 'r') as tempScaleFile:
+                main(dataFile,  rawXAccelFile,  accelScaleFile,  rawPressureFile,  pressureScaleFile,  tempOffsetFile,  rawTempFile,  tempScaleFile)
+#        wm = pyinotify.WatchManager()
+#        notifier = pyinotify.Notifier(wm, EventHandler())
+#        wm.add_watch('/sys/bus/iio/devices/iio:device1/', pyinotify.ALL_EVENTS)
+#        wm.add_watch('/sys/bus/iio/devices/iio:device2/', pyinotify.ALL_EVENTS)
+#        p = EventHandler("data.csv")
+#        notifier.loop()
     except KeyboardInterrupt:
         cleanup(dataFile)
